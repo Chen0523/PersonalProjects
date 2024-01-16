9b1fdf4d921d4462b99cd2df18ca3de8793ca738
Chen <cathyq0523@outlool.com>
2024-01-17 00:37:30 +0100

    added Book

--- src/components/current.js
+++ src/components/current.js
@@ -2,65 +2,182 @@ import React, { useEffect } from "react";
 import { Link } from "react-router-dom";
 import "./components.css";
 import Coocbooc from "../img/coocbooc5.png";
-import Tech from "../img/techtree.png";
+import EmmaReyes from "../img/EmmaReyes.png";
+import Aiice from "../img/AIICE.gif";
+import AiicePNG from "../img/aiice.png";
 
 const Current = () => {
-
   return (
     <div className="main_container">
-        <div className = "item">
-            <div className = "img">  <img src={Tech} alt=" Tech tree Illustration"></img></div>
-
-        <div className ="des">
-            <div className = "br">TechTree</div>
-            <ul>Status: <li>In development.
-                {/*Demo can be found <Link className = "linkStyle" to ={"/techTreeDemo"}> Hier -> </Link>*/}
-            </li></ul>
-            <ul>Persona:
-                <li> People who want to learn new skills and don't know where to start with or are overwhelmed by diverse online courses.</li>
-                <li> Young professionals who want to visualize their skills as a tech-tree.</li>
-            </ul>
-            <ul >Goal:
-                <li>A website where users can find the overview of the study routine for a certain skill such as web development.
-                Users can also find corresponding </li>
-            </ul>
+      <div className="item">
+        <div className="img">
+          {" "}
+          <img src={Tech} alt=" Tech tree Illustration"></img>
         </div>
 
+        <div className="des">
+          <div className="br">TechTree</div>
+          <ul>
+            Status:{" "}
+            <li>
+              {" "}
+              Building Demo and iterating the idea.
+              {/*Demo can be found <Link className = "linkStyle" to ={"/techTreeDemo"}> Hier -> </Link>*/}
+            </li>
+          </ul>
+          <ul>
+            Persona:
+            <li>
+              {" "}
+              People who want to get daily fashion inspiration from their
+              beloved books.
+            </li>
+            <li>
+              {" "}
+              Young professionals who want to visualize their skills as a
+              tech-tree.
+            </li>
+          </ul>
+          <ul>
+            Goal:
+            <li>
+              A website where users can find the overview of the study routine
+              for a certain skill such as web development. The routine will be
+              personalized depends on the goals and constrains of the user. The
+              web is also going to show the knowledge points that are covered in
+              the popular online courses.{" "}
+            </li>
+          </ul>
+        </div>
+      </div>
+      <div className="item">
+        <div className="double_img_H">
+          {" "}
+          <img src={AiicePNG} alt=" Tech tree Illustration"></img>
+          <img src={Aiice} alt=" Tech tree Illustration"></img>
+        </div>
 
+        <div className="des">
+          <div className="br">AIice in Labelland</div>
+          <ul>
+            Status:{" "}
+            <li>
+              {" "}
+              In Development.
+              {/*Demo can be found <Link className = "linkStyle" to ={"/techTreeDemo"}> Hier -> </Link>*/}
+            </li>
+          </ul>
+          <ul>
+            Description:
+            <li>
+              {" "}
+              A labeling game that helps with gathering data needed for my
+              master thesis "Position Optimization for Intelligent AR
+              Characters".
+            </li>
+            <li>
+              {" "}
+              This is an internal web application which "Gamify" the labeling
+              process to motivate my colleagues to help me with the label work.
+              To collect enough data to train a DNN, players are asked to choose
+              the character which stays at the position that is most comfortable
+              to them.
+            </li>
+          </ul>
+          <ul>
+            Game Mechanism:
+            <li>
+              {" "}
+              The story is inspired by "Alice in wonderland", in this game, our
+              princess AIice (Character image from Unity Chan!) is trapped in
+              the Labelland. The Labelland makes a fake AIice to confuse the
+              player who wants to rescue her. In order to rescue the real AIice,
+              the player should choose a role and throw their weapon to break
+              the curse.
+            </li>
+            <li>
+              {" "}
+              Different roles will have different weapons with different sound
+              effects, such as the cook will throw eyeball cupcakes.
+            </li>
+            <li> Players can label more to unlock new roles.</li>
+            <li>
+              {" "}
+              A scoreboard which shows the competition between players from
+              different countries will be added in the future.
+            </li>
+          </ul>
+
+          <ul>
+            {" "}
+            Remark:
+            <li>
+              {" "}
+              The monster characters used in this labeling game are based on the
+              images from Pinterest and processed in Adobe Illustrator.
+            </li>
+          </ul>
         </div>
-        <div className = "item">
-            <div className = "img">
-            <img src={Coocbooc} alt=" Book with Daly Eggs Illustration"></img>
+      </div>
+
+      <div className="item">
+        <div className="img">
+          <img src={Coocbooc} alt=" Book with Daly Eggs Illustration"></img>
         </div>
-        <div className ="des">
-            <div className = "br">COOCBOOC</div>
-            <ul>Status
-                <li> Currently closed. (Online from Aug 2020 - Jan 2021.)
-                    More Infos can be found <a style = {{color:"#4F4847", fontWeight:"bold"}} target="_blank" href="https://www.producthunt.com/posts/coocbooc-1">HIER</a>.
-                    </li>
-            </ul>
-            <ul >Persona:
-                <li>Young people who want to organize themed movie nights with snacks from a movie.</li>
-                <li>Moms who want to prepare interesting food for the kids.</li>
-                <li>Food bloggers who want to get inspired and reproduce the food in the books/movies.</li>
-            </ul>
-            <ul >Goal:
-                <li>A website where users can search recipes from a certain book/movie or just get inspired by looking around.
-                    Users can also upload their own recipes after a quick registration.</li>
-            </ul>
-            <ul >Personal Goal:
+        <div className="des">
+          <div className="br">COOCBOOC</div>
+          <ul>
+            Status
+            <li>
+              {" "}
+              Currently closed. (Online from Aug 2020 - Jan 2021.) More Infos
+              can be found{" "}
+              <a
+                style={{ color: "#4F4847", fontWeight: "bold" }}
+                target="_blank"
+                href="https://www.producthunt.com/posts/coocbooc-1"
+              >
+                HIER
+              </a>
+              .
+            </li>
+          </ul>
+          <ul>
+            Persona:
+            <li>
+              Young people who want to organize themed movie nights with snacks
+              from a movie.
+            </li>
+            <li>Moms who want to prepare interesting food for the kids.</li>
+            <li>
+              Food bloggers who want to get inspired and reproduce the food in
+              the books/movies.
+            </li>
+          </ul>
+          <ul>
+            Goal:
+            <li>
+              A website where users can search recipes from a certain book/movie
+              or just get inspired by looking around. Users can also upload
+              their own recipes after a quick registration.
+            </li>
+          </ul>
+          <ul>
+            Personal Goal:
             <li> To learn the skills of full-stack web-development.</li>
-            </ul>
-            <ul>Technology used:
-            <li> React, React Hooks, CSS, AWS Cloud, AWS Amplify, GraphQL, Facebook federated login, Google Analytics, Adobe Illustrator,
-            Design thinking, Graphic design</li>
-            </ul>
+          </ul>
+          <ul>
+            Skills used:
+            <li>
+              {" "}
+              Design thinking, Idea Iteration, Go To Market Planning, Graphic
+              Design, React, React Hooks, CSS, AWS Cloud, AWS Amplify, GraphQL,
+              Facebook federated login, Adobe Illustrator,
+            </li>
+          </ul>
         </div>
-
-        </div>
+      </div>
     </div>
   );
 };
 export default Current;
-
-
--- src/components/justForFun.js
+++ src/components/justForFun.js
@@ -1,84 +1,181 @@
-import React, { useEffect } from "react";
-import { Link } from "react-router-dom";
+import React, {useEffect} from "react";
+import {Link} from "react-router-dom";
 import "./components.css";
 import ArtodoB from "../img/artodoB.gif";
 import ArtodoW from "../img/artodoW.gif";
 import Robot from "../img/robot.gif";
 import Burnt from "../img/firstaidBurnt.gif";
+import Route from "../img/route.gif";
 import Fell from "../img/firstAidFell.gif";
-
+import Dice from "../img/dice2.png";
+import FingerMonkey from "../img/fingerMonkey.gif"
 const JustForFun = () => {
 
-  return (
-    <div className="main_container">
-        <div className="item">
-        <div className = "double_img">
-            <img src={Burnt} alt=" Process for helping others"></img>
-            <img src={Fell} alt=" Process for self help"></img>
-        </div>
-        <div className ="des">
-            <div className = "br">FirstAid for Self-help</div>
-            <ul>Goal:
-                <li>On this web, simply by specifying the emergency situation and marking the symptoms visually, users can find the correct first aid treatment before
-                they could get professional help.</li>
-            </ul>
-            <ul>Functions:
-                <li> The web will adjust suggestions based on the emergency situation and whether the user want to self help (shown in the right illustration)
-                    or to help others (shown in the left illustration).</li>
-                <li> Users can mark the hurt area freely on the human body illustration and document the symptoms.</li>
-                <li> Users will get advices (in video/voice/text form) for what to do before the professional help arrives.</li>
-            </ul>
-            <ul>
-                Technology used:
-                <li>Adobe XD, Adobe Illustrator</li>
-            </ul>
-        </div>
+    return (
+        <div className="main_container">
+            <div className="item">
+                <div className="img">
+                    <img src={Dice} alt=" Characters"></img>
+                </div>
+                <div className="des">
+                    <div className="br">Lord of Coffee</div>
+                    <ul>Goal:
+                        <li>A rapid but fun team game to decide who will make coffee/do the chores.</li>
+                    </ul>
+                    <ul>Game Mechanism:
+                        <li> Every player get two dices and choose a role. Every player plays three turns against each other.</li>
+                        <li> In each turn, two players throw their two dices at the same time.</li>
+                        <li> If the player has one dice that has points greater than three,
+                        he/she can decide whether he/she wants to apply their role's ability to the other dice or one of his enemy's dice.</li>
+                        <li> The one with greater points wins the turn.</li>
+
+                    </ul>
+                    <ul>
+                        Roles (Example):
+                        <li> Witch - Bewitch the enemy by making one of the enemy's dice roll in a random direction.</li>
+                        <li> Thief - Stoles one point from the enemy if the enemy activates his ability.</li>
+                        <li> Fighter - Adds one point to one of his dices. </li>
+                        <li> Dreamer - Make the dream come true - choose one dice to be as big as the other one. </li>
+                        <li> Judge - Judges that the enemy can only activate his ability when he has one dice with more than four points. </li>
+                        <li> ...</li>
+                    </ul>
+                    <ul>
+                        Technology used:
+                        <li>Game Design, Character Design, Adobe Illustrator</li>
+                    </ul>
+                </div>
+            </div>
+            <div className="item">
+                <div className="img">
+                    <img src={FingerMonkey} alt=" Finger Monkey"></img>
+                </div>
+                <div className="des">
+                    <div className="br">Finger Monkey</div>
+                    <ul>Description:
+                        <li>A mini but sweet bus game.</li>
+                    </ul>
+                    <ul>Game Mechanism:
+                        <li> Tired of pre-designed game levels? Use the city as your playground! </li>
+                        <li> When you sit on the bus, take out your phone, select one object in your camera as the obstacle you should avoid.</li>
+                        <li> Trees, street lights, people wearing a hat...You design your own level! </li>
+                        <li> What about the speed? You ask? Ops sorry! Your bus driver has it under control!</li>
+                    </ul>
+                    <ul>
+                        Technology used:
+                        <li>Game Design, Photoshop</li>
+                        <li>(This prototype is going to be implemented in Unity AR Foundation.)</li>
+                    </ul>
+                </div>
+            </div>
+
+            <div className="item">
+                <div className="double_img_V">
+                    <img src={Burnt} alt=" Process for helping others"></img>
+                    <img src={Fell} alt=" Process for self help"></img>
+                </div>
+                <div className="des">
+                    <div className="br">FirstAid for Self-help</div>
+                    <ul>Goal:
+                        <li>On this web, simply by specifying the emergency situation and marking the symptoms visually,
+                            users can find the correct first aid treatment before
+                            they could get professional help.</li>
+                    </ul>
+                    <ul>Functions:
+                        <li> The web will adjust suggestions based on the emergency situation and whether the user want
+                            to self help (shown in the right illustration)
+                            or to help others (shown in the left illustration).</li>
+                        <li> Users can mark the hurt area freely on the human body illustration and document the
+                            symptoms.
+                        </li>
+                        <li> Users will get advice (in video/voice/text form) on what to do before the professional
+                            help arrives.
+                        </li>
+                    </ul>
+                    <ul>
+                        Technology used:
+                        <li>Adobe XD, Adobe Illustrator</li>
+                    </ul>
+                </div>
 
 
-        </div>
-<div className = "item">
-        <div className = "double_img">
-            <img src={ArtodoB} alt=" Dark version"></img>
-            <img src={ArtodoW} alt=" Bright Verstion"></img>
-        </div>
-        <div className ="des">
-            <div className = "br">ArTodo List</div>
-            <ul>Goal:
-                <li>The app should motivate users to accomplish the tasks to color the illustration in the background.</li>
-            </ul>
-            <ul>Functions:
-                <li> The app will provide a new uncolored illustration everyday.</li>
-                <li> Users can finish the tasks to color the illustration in the background, and share the accomplishment to social media afterwards.</li>
-                <li> Users can combine their own color set or choose from the given color templates</li>
-            </ul>
-            <ul>
-                Technology used:
-                <li>Adobe XD, Adobe Illustrator</li>
-            </ul>
+            </div>
+            <div className="item">
+                <div className="double_img_V">
+                    <img src={ArtodoB} alt=" Dark version"></img>
+                    <img src={ArtodoW} alt=" Bright Verstion"></img>
+                </div>
+                <div className="des">
+                    <div className="br">ArTodo List</div>
+                    <ul>Goal:
+                        <li>The app should motivate users to accomplish the tasks to color the illustration in the
+                            background.</li>
+                    </ul>
+                    <ul>Functions:
+                        <li> The app will provide a new uncolored illustration every day.</li>
+                        <li> Users can finish their Todo tasks to color the illustration in the background, and share the
+                            accomplishment to social media afterward.
+                        </li>
+                        <li> Users can combine their own color set or choose from the given color templates</li>
+                    </ul>
+                    <ul>
+                        Technology used:
+                        <li>Adobe XD, Adobe Illustrator</li>
+                    </ul>
 
 
-        </div>
-</div>
-        <div className="item">
-        <div className = "img bomb">
-            <img src={Robot} alt=" bomberman"></img>
-        </div>
-        <div className ="des">
-            <div className = "br">Bomberman Robot</div>
+                </div>
+            </div>
+            <div className="item">
+                <div className="img bomb">
+                    <img src={Robot} alt=" bomberman"></img>
+                </div>
+                <div className="des">
+                    <div className="br">Bomberman Robot</div>
 
-            <ul >Uni Project:
-                <li>With the given Bomberman API, students can freely choose a technology to train a robot who could learn to play
-                    the Bomberman game without hardcoded rules.</li>
-            </ul>
+                    <ul>Semester Project:
+                        <li>The course <a style={{color: "#4F4847", fontWeight: "bold"}} target="_blank"
+                                    href="https://hci.iwr.uni-heidelberg.de/teaching/fundamentals_machine_learning_2017">Fundamentals of Machine Learning</a> provides
+                            an API for the game Bomberman, with which students shall train a robot who could learn to play the Bomberman game without hardcoded rules.
+                        The technology applied for the training shall be chosen among current reinforcement learning technologies.</li>
+                    </ul>
 
-            <ul>Technology used:
-            <li> Deep Learning, Deep Q Learning, Reinforce Learning, Pytorch, Feature extraction</li>
-            </ul>
-        </div>
+                    <ul>Technology used:
+                        <li> Deep Learning, Deep Q Learning, Reinforcement Learning, Pytorch, Feature extraction</li>
+                    </ul>
+                </div>
 
+            </div>
+            <div className="item">
+                <div className="img route">
+                    <img src={Route} alt=" route planner"></img>
+                </div>
+                <div className="des">
+                    <div className="br">Intelligent Route Planner</div>
+
+                    <ul>Idea for <a style={{color: "#4F4847", fontWeight: "bold"}} target="_blank"
+                                    href="https://home.kpmg/ch/de/home/karriere/ideation-challenge.html">KPMG Ideation
+                        Challenge 2019</a>
+
+                        <li> Intelligent AI route planner shall plan the route according to the user's condition
+                            (whether the user is wearing high heal shoes or has the need to keep
+                            the look tidy) and subjective constrains such as the current weather to plan the route
+                            dynamically to the last meter.
+                        </li>
+                    </ul>
+                    <ul>The full video can be found <a style={{color: "#4F4847", fontWeight: "bold"}} target="_blank"
+                                    href="https://drive.google.com/file/d/1roksu2WQXtkXhaZXdFphUptcgVj5aCyr/view?usp=sharing">HIER</a>:
+                        <li>To protect privacy, the team introduction part is cut out.</li>
+                    </ul>
+                    <ul>Technology used:
+                        <li> Krita (Animation Design), Brainstorming, Idea Generation, Team work </li>
+                    </ul>
+
+
+                </div>
+
+            </div>
         </div>
-    </div>
-  );
+    );
 };
 export default JustForFun;
 
